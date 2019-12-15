32d6308454b51d522f06947d9d2292fc2123d4ae
hsiaowj <B04702108@ntu.edu.tw>
2019-12-15 09:28:37 +0800

    Update Bus_Route.py
    直接利用路線特殊編號連到價格頁面

--- Bus_Route.py
+++ Bus_Route.py
@@ -10,8 +10,15 @@ data_topost = {'Type':'GetKey', 'Data':data_pickedroute, 'Lang':'#'}
 r_route = requests.post('https://www.taiwanbus.tw/APP_API/Test/ByLine.aspx', data=data_topost, verify=False)
 R_Route = [ r.strip('_') for r in r_route.text.split(',')]
 # print(R_Route)
-'''填入代號連道路線網站'''
-url_route = 'https://www.taiwanbus.tw/information.aspx?Lang=Cht&Line=' + str(R_Route[4])  # 這裡是採用去程:R[4]；如果回程:R[9]
-# print(url_route)
-new_page = webdriver.Chrome(executable_path ='C:\\Users\\Hsiao Wan-Ju\\Downloads\\chromedriver.exe')
-new_page.get(url_route)
+
+'''填入代號連到路線頁面'''
+url_route_page = 'https://www.taiwanbus.tw/information.aspx?Lang=Cht&Line=' + str(R_Route[4])  # 這裡是採用去程:R[4]；如果回程:R[9]
+# print(url_route_page)
+route_page = webdriver.Chrome(executable_path ='C:\\Users\\Hsiao Wan-Ju\\Downloads\\chromedriver.exe')
+route_page.get(url_route_page)
+
+'''填入代號連到價格頁面'''
+url_price_page = 'https://web.taiwanbus.tw/eBUS/subsystem/ticket/TMSquery.aspx?run_id=' + str(R_Route[4])  # 這裡是採用去程:R[4]；如果回程:R[9]
+# print(url_price_page)
+price_page = webdriver.Chrome(executable_path ='C:\\Users\\Hsiao Wan-Ju\\Downloads\\chromedriver.exe')
+price_page.get(url_price_page)
