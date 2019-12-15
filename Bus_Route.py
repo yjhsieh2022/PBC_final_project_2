7d96aca351efc6e3f7cd16a6226fb45047282997
hsiaowj <B04702108@ntu.edu.tw>
2019-12-15 20:32:04 +0800

    Update Bus_Route.py
    試爬價格

--- Bus_Route.py
+++ Bus_Route.py
@@ -14,11 +14,22 @@ R_Route = [ r.strip('_') for r in r_route.text.split(',')]
 '''填入代號連到路線頁面'''
 url_route_page = 'https://www.taiwanbus.tw/information.aspx?Lang=Cht&Line=' + str(R_Route[4])  # 這裡是採用去程:R[4]；如果回程:R[9]
 # print(url_route_page)
-route_page = webdriver.Chrome(executable_path ='C:\\Users\\Hsiao Wan-Ju\\Downloads\\chromedriver.exe')
-route_page.get(url_route_page)
+driver_route_page = webdriver.Chrome(executable_path ='C:\\Users\\Hsiao Wan-Ju\\Downloads\\chromedriver.exe')
+driver_route_page.get(url_route_page)
+
+
 
 '''填入代號連到價格頁面'''
 url_price_page = 'https://web.taiwanbus.tw/eBUS/subsystem/ticket/TMSquery.aspx?run_id=' + str(R_Route[4])  # 這裡是採用去程:R[4]；如果回程:R[9]
 # print(url_price_page)
-price_page = webdriver.Chrome(executable_path ='C:\\Users\\Hsiao Wan-Ju\\Downloads\\chromedriver.exe')
-price_page.get(url_price_page)
+driver_price_page = webdriver.Chrome(executable_path ='C:\\Users\\Hsiao Wan-Ju\\Downloads\\chromedriver.exe')
+driver_price_page.get(url_price_page)
+
+html_price_page = driver_price_page.page_source
+# driver_price_page.close()  # 先不關
+
+soup_price_page = BeautifulSoup(html_price_page, 'html.parser')
+attr_price_page = {'valign':'middle'}
+price_tags = soup_price_page.find_all('td', attrs=attr_price_page)
+for tag in price_tags:
+    print(tag.get_text())
