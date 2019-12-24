89d9056e6145ec3a10134ff5d3cc32dc4c983999
hsiaowj <B04702108@ntu.edu.tw>
2019-12-24 15:56:48 +0800

    official web framework
    首頁草稿(py、html檔)

---
+++ official/official_web.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+taiwango = Flask(__name__)
+
+@taiwango.route('/', methods=['GET', 'POST'])  # 佳妤，taiwango 首頁
+def home():
+    if request.method == 'POST':
+        return redirect(url_for('routechoices'))
+
+    return render_template('web_page1.html')
